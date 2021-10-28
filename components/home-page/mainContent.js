c76a9a48dc017568c03ff8cf0549b4b6e579a688
CSpiegelhalter <spiegelhaltercurt@gmail.com>
2021-10-28 13:17:15 -0500

    Added new section beneath hero. Added styling to other components

--- components/home-page/mainContent.js
+++ components/home-page/mainContent.js
@@ -3,9 +3,16 @@ import classes from './mainContent.module.css'
 function MainContent() {
     return(
         <section className={classes.about}>
-            <h1 className={classes.p}>
+            <h1 className={classes.tit}>
                 The very best lawn care service in Pensacola.
             </h1>
+            
+            <div className={classes.start}>
+                <h2 className={classes.tit}>Get started in 2 easy stepped</h2>
+                <div>
+                    <h3>Call for a quote</h3>
+                </div>
+            </div>
         </section>
     )
 }
