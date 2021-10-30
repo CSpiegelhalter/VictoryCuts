5666ed3ece76cab09f2eefcbd1ff2e74a614c055
CSpiegelhalter <spiegelhaltercurt@gmail.com>
2021-10-29 22:48:28 -0500

    Added styling to icon div. Added a new services provided divwith some styling. Had to change height of hero image.

--- components/home-page/mainContent.js
+++ components/home-page/mainContent.js
@@ -12,7 +12,7 @@ function MainContent() {
             </h1>
             <div className={classes.line}></div>
             <div className={classes.start}>
-                <h2 className={classes.tit}>Get started in 2 easy steps</h2>
+                <h2 className={classes.tit2}>Get started in 2 easy steps</h2>
                 <div className={classes.containsection}>
                     <div className={classes.sections}>
                         <h3>Call for a quote</h3>
@@ -20,6 +20,7 @@ function MainContent() {
                             <Image src={quotation} alt='Call for quote' />
                         </div>
                     </div>
+                    <div className={classes.vl}></div>
                     <div className={classes.sections}>
                         <h3>Recieve Service</h3>
                         <div className={classes.icons}>
@@ -27,6 +28,32 @@ function MainContent() {
                         </div>
                     </div>
 
+                </div>
+            </div>
+
+            <div>
+                <div className={classes.why}>
+                    <h2 className={classes.head}>Our services:</h2>
+                    <div className={classes.services}>
+                        <ul className={classes.each}>
+                            <li>Lawn Mowing</li>
+                            <li>Landscaping</li>
+                            <li>Lawn Aeration</li>
+                            <li>Lawn Winterization</li>
+                        </ul>
+                        <ul className={classes.each}>
+                            <li>Planting</li>
+                            <li>Lawn Fertilizing</li>
+                            <li>Seeding</li>
+                            <li>Leaf Removal</li>
+                        </ul>
+                        <ul className={classes.each}>
+                            <li>Raking Services</li>
+                            <li>Top Soil/Fill Dirt</li>
+                            <li>Weeding</li>
+                            <li>Tree Trimming</li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         </section>
