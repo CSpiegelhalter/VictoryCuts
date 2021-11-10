5f97f7afe5bd4cc96d2481cc70ea7d42530807ee
CSpiegelhalter <spiegelhaltercurt@gmail.com>
2021-11-10 10:53:34 -0600

    Trying to fix responsivness

--- components/home-page/mainContent.js
+++ components/home-page/mainContent.js
@@ -33,50 +33,49 @@ function MainContent() {
                 <div className={classes.why}>
                     <h2 className={classes.head}>Services we provide:</h2>
                     <div className={classes.contain}>
-                    <div className={classes.services}>
-                        <div className={classes.left}>
-                            <img className={classes.examples} src={'../../lawn2.webp'} alt={'Lawn Mowing'}/>
-                            <img className={classes.responsive} src={'../../responsive-5/lawn2.webp'} alt={'Lawn Mowing'}/>
-                            <img className={classes.responsive1} src={'../../responsive/lawn2.webp'} alt={'Lawn Mowing'}/>
-                            <img className={classes.responsivelast} src={'../../responsive-last/lawn2.webp'} alt={'Lawn Mowing'}/>
-                            <img className={classes.responsivetiny} src={'../../responsive-tiny/lawn2.webp'} alt={'Lawn Mowing'}/>
-                            <ul className={classes.eachL}>
-                                <li>Lawn Mowing</li>
-                                <li>Edging</li>
-                                <li>Lawn Aeration</li>
-                                <li>Hedge Trimming</li>
-                                <li>Fertilization</li>
-                            </ul>
+                        <div className={classes.services}>
+                            <div className={classes.left}>
+                                <img className={classes.examples} src={'../../lawn2.webp'} alt={'Lawn Mowing'} />
+                                <img className={classes.responsive} src={'../../responsive-5/lawn2.webp'} alt={'Lawn Mowing'} />
+                                <img className={classes.responsive1} src={'../../responsive/lawn2.webp'} alt={'Lawn Mowing'} />
+                                <ul className={classes.each}>
+                                    <li>Landscaping</li>
+                                    <li>Planting</li>
+                                    <li>Lawn Fertilizing</li>
+                                    <li>Leaf Removal</li>
+                                    <li>Area Prep</li>
+                                </ul>
+                                <img className={classes.examples} src={'../../hedges.webp'} alt={'Hedge Trimming'} />
+                                <img className={classes.responsive} src={'../../responsive-5/hedges.webp'} alt={'Hedge Trimming'} />
+                                <img className={classes.responsive1} src={'../../responsive/hedges.webp'} alt={'Hedge Trimming'} />
+                            </div>
+
+
+                            <div className={classes.right}>
+
+
+                                <ul className={classes.each}>
+                                    <li>Lawn Mowing</li>
+                                    <li>Edging</li>
+                                    <li>Lawn Aeration</li>
+                                    <li>Hedge Trimming</li>
+                                    <li>Fertilization</li>
+                                </ul>
+
+                                <img className={classes.examples} src={'../../landscaping.webp'} alt={'Landscaping'} />
+                                <img className={classes.responsive1} src={'../../responsive/landscaping.webp'} alt={'Landscaping'} />
+                                <img className={classes.responsive} src={'../../responsive-5/landscaping.webp'} alt={'Landscaping'} />
+
+                                <ul className={classes.each}>
+                                    <li>Weed-eating</li>
+                                    <li>Raking Services</li>
+                                    <li>Weeding</li>
+                                    <li>Tree Trimming</li>
+                                    <li>Basic Gardening</li>
+                                </ul>
+
+                            </div>
                         </div>
-                        <div className={classes.right}>
-                            <ul className={classes.eachR}>
-                                <li>Landscaping</li>
-                                <li>Planting</li>
-                                <li>Lawn Fertilizing</li>
-                                <li>Leaf Removal</li>
-                                <li>Area Prep</li>
-                            </ul>
-                            <img className={classes.examples} src={'../../landscaping.webp'} alt={'Landscaping'} />
-                            <img className={classes.responsive1} src={'../../responsive/landscaping.webp'} alt={'Landscaping'} />
-                            <img className={classes.responsive} src={'../../responsive-5/landscaping.webp'} alt={'Landscaping'} />
-                            <img className={classes.responsivelast} src={'../../responsive-last/landscaping.webp'} alt={'Landscaping'} />
-                            <img className={classes.responsivetiny} src={'../../responsive-tiny/landscaping.webp'} alt={'Landscaping'} />
-                        </div>
-                        <div className={classes.left}>
-                        <img className={classes.examples} src={'../../hedges.webp'} alt={'Hedge Trimming'}/>
-                        <img className={classes.responsive} src={'../../responsive-5/hedges.webp'} alt={'Hedge Trimming'}/>
-                        <img className={classes.responsive1} src={'../../responsive/hedges.webp'} alt={'Hedge Trimming'}/>
-                        <img className={classes.responsivelast} src={'../../responsive-last/hedges.webp'} alt={'Hedge Trimming'}/>
-                        <img className={classes.responsivetiny} src={'../../responsive-tiny/hedges.webp'} alt={'Hedge Trimming'}/>
-                            <ul className={classes.eachL}>
-                                <li>Weed-eating</li>
-                                <li>Raking Services</li>
-                                <li>Weeding</li>
-                                <li>Tree Trimming</li>
-                                <li>Basic Gardening</li>
-                            </ul>
-                        </div>
-                    </div>
                     </div>
                 </div>
             </div>
