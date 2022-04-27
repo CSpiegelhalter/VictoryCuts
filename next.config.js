d62b904a32fa73d00da19db720132abb7b6f3fe2
CSpiegelhalter <spiegelhaltercurt@gmail.com>
2022-04-26 21:22:11 -0500

    Edited next config files to hopefully improve performance

--- next.config.js
+++ next.config.js
@@ -1,15 +1,34 @@
 const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 })
 
-module.exports = [
-withBundleAnalyzer({
+module.exports = {
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-    config.optimization.minimizer = [];
-       config.optimization.minimizer.push(new OptimizeCSSAssetsPlugin({}));
+    config.plugins.push(new OptimizeCSSAssetsPlugin({
+      assetNameRegExp: /\.optimize\.css$/g,
+      cssProcessor: require('cssnano'),
+      cssProcessorPluginOptions: {
+        preset: ['default', { discardComments: { removeAll: true } }],
+      },
+      canPrint: true
+    }))
+    config.plugins.push(new UglifyJsPlugin({
+      uglifyOptions: {
+        exclude: /react\.js/,
+        parallel: true,
+        cache: true,
+      },
+    }))
+    
+
+
+
     return config;
-  },
-})
-]
+
+  }
+
+
+}
