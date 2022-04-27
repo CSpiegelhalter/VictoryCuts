92d4b4a6427463826cbe2f6b2289abcddda3f551
CSpiegelhalter <spiegelhaltercurt@gmail.com>
2022-04-26 21:31:34 -0500

    Improving performance

--- next.config.js
+++ next.config.js
@@ -22,9 +22,19 @@ module.exports = {
         cache: true,
       },
     }))
-    
 
 
+    if (!isServer) {
+      config.optimization.splitChunks.cacheGroups = {
+        ...config.optimization.splitChunks.cacheGroups,
+        '@sentry': {
+          test: /[\\/]node_modules[\\/](@sentry)[\\/]/,
+          name: '@sentry',
+          priority: 10,
+          reuseExistingChunk: false,
+        },
+      };
+    }
 
     return config;
 
